da3e7146f3841e979288e33531e178b95f13fbb0
Helinton Mello <helinton.070398@bento.local>
2024-02-29 21:53:41 -0300

    App.js

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
 export default function App() {
+  const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
+  useEffect(() => {
+    setCalculation(() => count * 2);
+  }, [count]);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Você clicou {count} vez(es)</Text>
+      <Button title="Clique Aqui" onPress={() => setCount((c) => c + 1)} />
+      <Text>Cálculo: {calculation}</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
